e5e2413b978c00c4b00cf25569dc3c6de14af929
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-02-16 23:34:56 -0200

    Include Project on Git Hub
    classes and criation of the JSON for question.

---
+++ Project/Back_End/SkillsAssessmentServer/src/Model.java
@@ -0,0 +1,58 @@
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
+
+public class Model{
+	
+	ObjectContainer students = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "../competencies.db4o");
+	ObjectContainer questions = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "../questions.db4o");
+	
+	public void addStudent(Student student){
+		students.store(student);
+	}
+	
+	public void addQuestion(Question question){
+		questions.store(question);
+	}
+	
+	
+	public Student searchStudentByRA(int ra){
+		
+		
+		Query query = students.query();
+		query.constrain(Student.class);
+	    ObjectSet<Student> allStudents = query.execute();
+		
+	    for(Student student:allStudents){
+	    	if(student.getRa()==ra){
+	    		return student;
+	    	}
+	    	
+	    }
+	    
+	    return null;
+
+		
+	}
+	
+	public Question searchQuestionByCode(int code){
+		
+		Query query = questions.query();
+		query.constrain(Question.class);
+	    ObjectSet<Question> allQuestions = query.execute();
+		
+	    for(Question question:allQuestions){
+	    	if(question.getNumber() == code){
+	    		return question;
+	    	}
+	    	
+	    }
+	    
+	    return null;
+		
+	}
+	
+
+}
---
+++ Project/Back_End/SkillsAssessmentServer/src/REST.java
@@ -0,0 +1,93 @@
+import static spark.Spark.get;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class REST {
+
+	private Model model;
+
+	public REST(Model store) {
+		this.model = store;
+	}
+
+	public void getStudentCompetencies() {
+
+		get(new Route("/competencies/:ra") {
+			@Override
+			public Object handle(Request request, Response response) {
+
+				Integer ra = Integer.parseInt(request.params(":ra"));
+
+				try {
+					Student student = model.searchStudentByRA(ra);
+
+					JSONArray jsonResult = new JSONArray();
+					JSONObject jsonObj = new JSONObject();
+
+					jsonObj.put("name", student.getName());
+					jsonObj.put("lidership", student.getCompetencies().getLeadership());
+					jsonObj.put("communication", student.getCompetencies().getCommunication());
+					jsonObj.put("values", student.getCompetencies().getValues());
+					jsonObj.put("workGroup", student.getCompetencies().getWorkGroup());
+					jsonObj.put("determination", student.getCompetencies().getDetermination());
+					jsonObj.put("resilience", student.getCompetencies().getResilience());
+					jsonObj.put("autonomy", student.getCompetencies().getAutonomy());
+
+					jsonResult.put(jsonObj);
+
+					return jsonResult;
+
+				} catch (JSONException e) {
+
+					e.printStackTrace();
+				}
+
+				return null;
+
+			}
+
+		});
+
+	}
+
+	public void getQuestionByNumber() {
+		get(new Route("/questions/:number") {
+			@Override
+			public Object handle(Request request, Response response) {
+				Integer number = Integer.parseInt(request.params(":number"));
+				try {
+					Question question = model.searchQuestionByCode(number);
+					
+					JSONArray jsonResult = new JSONArray(); // first - MAIN
+					JSONObject jsonObjQuestion = new JSONObject();
+										
+					jsonObjQuestion.put("number", question.getNumber());
+					jsonObjQuestion.put("introduction", question.getIntroduction());
+					jsonObjQuestion.put("question", question.getQuestion());			
+					
+					jsonObjQuestion.put("answers", question.getAnswers());
+							
+					jsonResult.put(jsonObjQuestion);
+					
+					return jsonResult;
+
+				} catch (JSONException e) {
+
+					e.printStackTrace();
+				}
+
+				return null;
+
+			}
+
+		});
+
+	}
+
+}
