f417276c3e3cad5d64b90a52215db85b4ed28447
Eduardo Alves Di Nizo <eduardodinizo@hotmail.com>
2016-02-20 16:42:43 -0200

    Update

--- Project/Back_End/SkillsAssessmentServer/src/br/com/fatec/dao/DaoUser.java
+++ Project/Back_End/SkillsAssessmentServer/src/br/com/fatec/dao/DaoUser.java
@@ -1,20 +1,13 @@
 package br.com.fatec.dao;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import com.mysql.jdbc.PreparedStatement;
-import br.com.fatec.connection.ConnectionMySql;
 import br.com.fatec.model.User;
 
 public class DaoUser {
-	
+
+
 	public static User getLogin(String userName, String password){
 		User teste = new User(); //
 		return teste;
 	}
-	
-	
 
 }
