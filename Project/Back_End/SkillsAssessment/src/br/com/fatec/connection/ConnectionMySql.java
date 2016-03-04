07b0d03a3f2ef0bedb2c8c348896cd7e305ab005
Eduardo Alves Di Nizo <eduardodinizo@hotmail.com>
2016-03-03 22:06:06 -0300

    commit

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/connection/ConnectionMySql.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/connection/ConnectionMySql.java
@@ -1,9 +1,12 @@
 package br.com.fatec.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.mysql.jdbc.Statement;
 
 public class ConnectionMySql {
 
@@ -13,7 +16,11 @@ public class ConnectionMySql {
     private static String ip="localhost:3306/SkillsAssessment?useSSL=false";
     private static String driver="com.mysql.jdbc.Driver";
     private static Connection connection = null;
-
+    private static Statement statement = null;
+    private static ResultSet resultset = null;
+    
+    public ConnectionMySql(){};
+    
     //padrao singleton
     public static Connection getConnection() {
         System.out.println(">>Connecting to database");
@@ -28,13 +35,30 @@ public class ConnectionMySql {
             throw new RuntimeException(e);    
         }catch (SQLException e) {
 
-            closeConnection();
+            close();
             throw new RuntimeException(e);
         }
 
     }
 
-    public static void closeConnection(){
+    public boolean conect() throws SQLException{
+    	boolean error = true;
+    	try{
+    		Class.forName(driver);
+    	}catch(ClassNotFoundException ex){
+    		Logger.getLogger(ConnectionMySql.class.getName()).log(Level.SEVERE,null, ex);
+            error = false;
+    	}
+    	
+    	// realizando conexão
+        connection = (Connection) DriverManager.getConnection("jdbc:mysql://"+ip+"",user, password);
+        // criando canal para execução de sql
+        statement = (Statement) connection.createStatement();
+        // retorna resultado da conexão
+        return error;    	
+    }
+    
+    public static void close(){
         try{
             if(connection!=null && !connection.isClosed()){
                 connection.close();
@@ -44,9 +68,44 @@ public class ConnectionMySql {
             e.printStackTrace();
         }
     }
+    
+    //select
+    public boolean executeQuery(String query) throws SQLException{
+    	resultset = statement.executeQuery(query); //executa sql
+        if (resultset.next()) { //aponta para primeiro registro da consulta
+            return true; // não há registros
+        } else {
+            return false;
+        }
+    }
+    
+    public ResultSet returnRegister() throws SQLException {
+        return resultset;
+    }
+    
+    public String returnField(String campo) throws SQLException {
+        String data;
+        data = resultset.getString(campo);
+        return data;
+    }
+    
+    public boolean nextRegister() throws SQLException {
+        return resultset.next();
+    }
+    
+    //INSERT, UPDATE E DELETE
+    public boolean executeSql(String SQL) throws SQLException {
+        int i = statement.executeUpdate(SQL);
+        if (i == 0) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
 
 	public static java.sql.Connection restartConnection() {
-		closeConnection();
+		close();
 		return ConnectionMySql.getConnection();
 	}
 
--- Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoCourse.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoCourse.java
@@ -12,28 +12,44 @@ import br.com.fatec.connection.ConnectionMySql;
 import br.com.fatec.model.course.Course;
 
 public class DaoCourse {
-	
- 	
+
+	public static boolean deleteCourse(Long code) throws SQLException {
+		Connection conn = ConnectionMySql.getConnection();
+		String sql = "DELETE FROM COURSE WHERE CRS_CODE = " + code + ";";
+		PreparedStatement cmd;
+		cmd = (PreparedStatement) conn.prepareStatement(sql);
+
+		if (cmd.execute()) {
+			cmd.close();
+			return true;
+		} else {
+			cmd.close();
+			return false;
+		}
+	}
+
 	public static List<Course> searchAllCourse() throws SQLException {
 		List<Course> listCourse = new ArrayList<>();
-		Connection conn = ConnectionMySql.getConnection();
+		ConnectionMySql connection = new ConnectionMySql();
 		String query = "select * from course;";
-		PreparedStatement cmd;
-		cmd = (PreparedStatement) conn.prepareStatement(query);
-		ResultSet rs = cmd.executeQuery();
+		if (connection.executeQuery(query)) {
+			do {
+				Course course = new Course();
+				course.setCode(Long.parseLong(connection.returnField("CRS_CODE")));
+				course.setName(connection.returnField("CRS_NAME"));
+				course.setSituation(Integer.parseInt(connection.returnField("CRS_SITUATION")));
+				course.setRegistration_date(connection.returnField("CRS_REGISTRATION_DATE"));
 
-		while (rs.next()) {
-			Course course = new Course();
-			course.setCode(Long.parseLong(rs.getString("CRS_CODE")));
-			course.setName(rs.getString("CRS_NAME"));
-			course.setSituation(Integer.parseInt(rs.getString("CRS_SITUATION")));			
-			course.setRegistration_date(rs.getString("CRS_REGISTRATION_DATE"));
-			
-			listCourse.add(course);
+				listCourse.add(course);
+			} while (connection.nextRegister());
+			connection.close();
+			return listCourse;
+		}else{
+			connection.close();
+			return null;
 		}
 		
-		rs.close();
-		return listCourse;
+
 	}
-	
+
 }
