a20ccae5e693778b8ffb979d7f8b78a7c1b5bf3d
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-09 14:05:47 -0300

    Creation of the Class Period

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/model/period/Period.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/model/period/Period.java
@@ -1,5 +1,42 @@
 package br.com.fatec.model.period;
 
-public class Period {
+import br.com.fatec.model.course.Course;
 
+public class Period extends Course{
+	
+	private Long code;
+	private int year;
+	private int period;
+	
+	public Period(){};
+	
+	public Period(Long code, int year, int period) {
+		this.code = code;
+		this.year = year;
+		this.period = period;
+	}
+
+	public Long getCode() {
+		return code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}	
 }
