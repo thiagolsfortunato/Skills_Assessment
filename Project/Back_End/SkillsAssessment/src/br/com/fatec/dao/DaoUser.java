65b0f3b2bc39104073e417056efabaee28b731f4
Eduardo Alves Di Nizo <eduardodinizo@hotmail.com>
2016-03-11 16:30:23 -0300

    changes in DaoUser

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoUser.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoUser.java
@@ -1,59 +1,69 @@
 package br.com.fatec.dao;
 
-import java.sql.Connection;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import com.mysql.jdbc.PreparedStatement;
 import br.com.fatec.connection.ConnectionMySql;
-import br.com.fatec.entity.TokenInfo;
 import br.com.fatec.entity.User;
 
 public class DaoUser {
 
 	@SuppressWarnings("finally")
-	public static User getLogin(String email, String password) throws SQLException {
-		ConnectionMySql connection = new ConnectionMySql();
-		User user = new User();
-		
-		ResultSet rs = null;
-		try {
-			String query = "select usr_kind, usr_code, usr_token from user where usr_username = '" + email + "' and usr_password = '" + password + "';";
-			PreparedStatement cmd;
-			cmd = (PreparedStatement) conn.prepareStatement(query);
-			rs = cmd.executeQuery();
-			if (rs != null) {
-				rs.next();
-				String token = updateTokenUser(rs.getString("USR_CODE"));
-				user.setKindPerson(rs.getString("USR_KIND"));
-				user.setUserCode(Long.parseLong(rs.getString("USR_CODE")));
-				user.setToken(token); 
-			}
+	public static User getLogin(String email, String password) throws SQLException{
+		ConnectionMySql conn = new ConnectionMySql();
+		User user = null;
 
+		try {
+			String query = "select usr_kind, usr_code, usr_token from user where usr_username = ? and usr_password = ?;";
+			
+			conn.conect();
+			conn.setStatement(conn.getConnection().prepareStatement(query));
+			conn.getStatement().setString(1, email);
+			conn.getStatement().setString(2, password);
+			
+			if (conn.executeQuery()) user = buildLogin(conn.returnRegister());
+			
 		} catch (SQLException e) {
-			// TODO: handle exceptions
-		} finally {
-			rs.close();
+			throw new RuntimeException(e);
+		} finally {		
 			conn.close();
 			return user;
 		}
 	}
 
+	@SuppressWarnings("finally")
 	private static String updateTokenUser(String id) throws SQLException {
-		ConnectionMySql connection = new ConnectionMySql();
-		String tk = Token.createJsonWebToken(id, (long) 1);
-		String query = "UPDATE USER SET usr_token=?  WHERE usr_code=?";
-
-		PreparedStatement cmd;
-		cmd = (PreparedStatement) conn.prepareStatement(query);
-		cmd.setString(1, tk);
-		cmd.setString(2, id);
-		int rowsUpdated = cmd.executeUpdate();
-		if (rowsUpdated > 0){
-			System.out.println("An existing user was updated successfully!");
-			return tk;
+		ConnectionMySql conn = new ConnectionMySql();
+		String token = null;
+		try {
+			token = Token.createJsonWebToken(id, (long) 1);
+			String query = "UPDATE USER SET usr_token=?  WHERE usr_code=?";
+			conn.conect();
+			conn.setStatement(conn.getConnection().prepareStatement(query));
+			conn.getStatement().setString(1, token);
+			conn.getStatement().setString(2, id);
+			
+			if(conn.executeSql()){
+				System.out.println("An existing user was updated successfully!");
+				return token;
+			}
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			conn.close();
+			return token;
 		}
-		return null;
+	}
+	
+
+	private static User buildLogin(ResultSet rs) throws SQLException {
+		User user = new User();
+		String token = updateTokenUser(rs.getString("USR_CODE"));
+		user.setKindPerson(rs.getString("USR_KIND"));
+		user.setUserCode(Long.parseLong(rs.getString("USR_CODE")));
+		user.setToken(token); 
+		return user;
 	}
 
 }
