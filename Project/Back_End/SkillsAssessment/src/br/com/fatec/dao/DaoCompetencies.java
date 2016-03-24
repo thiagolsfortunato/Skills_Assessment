5d211a3677c6d592bcb1e04b52c31a69fda590ff
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-23 21:59:15 -0300

    Update Class DaoCompetencies

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoCompetencies.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoCompetencies.java
@@ -1,6 +1,7 @@
 package br.com.fatec.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.fatec.connection.ConnectionMySql;
@@ -8,7 +9,7 @@ import br.com.fatec.entity.Competence;
 
 public class DaoCompetencies {
 
-	// Insert Competence
+	// Insert Competence - FUNCIONANDO!!
 	@SuppressWarnings("finally")
 	public static boolean insertCompetence(Competence competence) throws SQLException {
 		ConnectionMySql connection = new ConnectionMySql();
@@ -21,7 +22,7 @@ public class DaoCompetencies {
 			connection.getStatement().setString(1,String.valueOf(competence.getCode()));
 			connection.getStatement().setString(2,competence.getType());
 			connection.getStatement().setString(3,competence.getRegister());
-			connection.getStatement().setString(4,competence.getRegister());//wrong
+			connection.getStatement().setString(4,String.valueOf(competence.getWeight()));
 			
 			if (connection.executeSql()) {
 				insert = true;
@@ -29,43 +30,38 @@ public class DaoCompetencies {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
-			connection.getStatement().close();
 			connection.close();
 			return insert;
 		}
 	}
 	
-	// Delete Competence
+	// Delete Competence - FUNCIONANDO!!
 	@SuppressWarnings("finally")
 	public static boolean deleteCompetence(Long code) throws SQLException {
 		ConnectionMySql connection = new ConnectionMySql();
-		String sql = "delete from competence where com_code = ?;";
-		Competence competence = new Competence();
-		
+		String sql = "delete from competence where com_code = ?;";		
 		boolean delete = false;
 		try {
 			connection.conect();
 			connection.setStatement(connection.getConnection().prepareStatement(sql));
-			connection.getStatement().setString(1,String.valueOf(competence.getCode()));
+			connection.getStatement().setString(1,String.valueOf(code));
 			if (connection.executeSql()) {
 				delete = true;
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
-			connection.getStatement().close();
 			connection.close();
 			return delete;
 		}
 	}
 
-	// Search competence by name
+	// Search competence by code - FUNCIONANDO!!
 	@SuppressWarnings("finally")
 	public static Competence searchCompetenceByCode(Long code) throws SQLException {
 		Competence competence = new Competence();
 		ConnectionMySql connection = new ConnectionMySql();
-		String query = "select com_code, com_type, com_registration_date, com_situation"
-				+ "from competence where com_code = ?'";
+		String query = "select * from competence where com_code = ?";
 		try {
 			connection.conect();
 			connection.setStatement(connection.getConnection().prepareStatement(query));
@@ -79,19 +75,16 @@ public class DaoCompetencies {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
-			//connection.getResultset().close();
-			connection.getStatement().close();
 			connection.close();
 			return competence;
 		}
 	}
 
-	// Update Competence
+	// Update Competence - FUNCIONANDO!!!
 	@SuppressWarnings("finally")
-	public static boolean updateCompetence(Competence competence, Long code) throws SQLException {
+	public static boolean updateCompetence(Competence competence) throws SQLException {
 		ConnectionMySql connection = new ConnectionMySql();
-		String sql = "update competence set COM_CODE=?, COM_TYPE= ?, COM_REGISTRATION_DATE= ?,"
-				+ "COM_SITUATION=? where com_code=?;";
+		String sql = "update competence set COM_CODE=?, COM_TYPE= ?, COM_REGISTRATION_DATE= ? where COM_CODE=?;";
 		boolean update = false;
 		try {
 			connection.conect();
@@ -100,23 +93,22 @@ public class DaoCompetencies {
 			connection.getStatement().setString(2,competence.getType());
 			connection.getStatement().setString(3,competence.getRegister());
 			//connection.getStatement().setString(4,String.valueOf(competence.getSituation()));
-			connection.getStatement().setString(5, String.valueOf(code));
+			connection.getStatement().setString(4, String.valueOf(competence.getCode()));
 			if (connection.executeSql()) {
 				update = true;
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
-			connection.getStatement().close();
 			connection.close();
 			return update;
 		}
 	}
 		
-	// Search all competences - MEXER AQUI !!!!
-	@SuppressWarnings({ "finally", "null" })
+	// Search all competences - FUNCIONANDO !!!!
+	@SuppressWarnings({ "finally" })
 	public static List<Competence> searchAll() throws SQLException {
-		List<Competence> listCompetence = null;
+		List<Competence> listCompetence = new ArrayList<>();
 		String sql = "select * from COMPETENCE ;";
 		ConnectionMySql connection = new ConnectionMySql();
 		try {
@@ -133,12 +125,11 @@ public class DaoCompetencies {
 				} while (connection.nextRegister());
 			}
 		} catch (SQLException e) {
+			e.printStackTrace();
 			throw new RuntimeException(e);
 		} finally {
-			connection.returnRegister().close();
-			connection.getStatement().close();
 			connection.close();
 			return listCompetence;
 		}
-	}	
+	}
 }
