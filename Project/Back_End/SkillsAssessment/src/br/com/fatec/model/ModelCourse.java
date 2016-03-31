3bd7a785ecde863ae71647c54bd8a70e356aea02
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-30 22:53:54 -0300

    Update Class Course

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/model/ModelCourse.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/model/ModelCourse.java
@@ -7,15 +7,32 @@ import br.com.fatec.entity.Course;
 
 public class ModelCourse {
 
+	@SuppressWarnings("finally")
 	public boolean insertCourse(Course course) {
-		return DaoCourse.insertCourse(course);
+		boolean insert = false;
+		try{
+			insert = DaoCourse.insertCourse(course);
+			return insert;
+		}catch (SQLException e) {
+			System.out.println("Will not it was possible to enter the Course");
+		}finally {
+			return insert;
+		}		
 	}
 
+	@SuppressWarnings("finally")
 	public boolean updateCourse(Course course) {
-		return DaoCourse.updateCourse(course);
+		boolean update = false;
+		try{
+			update = DaoCourse.updateCourse(course);
+		}catch (SQLException e) {
+			System.out.println("Will not it was possible to delete the Course");
+		}finally {
+			return update;
+		}
 	}
 
-	public boolean deleteCourse(Long code) {
+	public boolean deleteCourse(Long code) throws SQLException {
 		return DaoCourse.deleteCourse(code);
 	}
 
