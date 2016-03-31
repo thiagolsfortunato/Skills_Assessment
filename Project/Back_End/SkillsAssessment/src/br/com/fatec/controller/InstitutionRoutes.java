d4eb292707c619a8897cdd3e3bbc2ce24ff95c49
Marcelo-Inacio <marcelomcl_10@hotmail.com>
2016-03-31 01:06:05 -0300

    implementation class InstitutionRoutes
    by Marcelo
    Willian

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/controller/InstitutionRoutes.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/controller/InstitutionRoutes.java
@@ -1,10 +1,12 @@
 package br.com.fatec.controller;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import com.google.gson.Gson;
 
 import br.com.fatec.commons.JsonUtil;
 import br.com.fatec.entity.Institution;
@@ -14,50 +16,98 @@ public class InstitutionRoutes {
 	
 	public static void getInstitution(){
 		ModelInstitution model = new ModelInstitution();
-		 
-		get("/Institution/find", (req, res) -> {
-			Long code = Long.parseLong( req.queryParams("code") );
-			String name =  req.queryParams("name") ;
-			Map<Integer, String> mapa = new HashMap<Integer, String>();
-			
-			if(  name!=null ){
-				List<Institution> fatecs = model.searchInstitutionByName(name);
-				return fatecs;
-			}else if( (code!=null) && (name==null) ){
-				Institution fatec = model.searchInstitutionByCode(code);
-				return fatec;
-			}else{	
-				mapa.put(404, "nenhuma fatec encontrada");
-				return mapa;
-			}
-			 
-			
-		}, JsonUtil.json());
 		
-		get("/Institution/delete", (req, res) -> {
-			Map<Integer, String> mapa = new HashMap<Integer, String>();
-			Long code = Long.parseLong( req.queryParams("code") );
-			boolean sucess = model.deleteInstitution(code);
+		post("/institution/insert", (req, res) -> {
+			String data = req.body();
+			Gson gson = new Gson();
+			Institution fatec = gson.fromJson(data, Institution.class);
+	
+			boolean sucess = model.insertInstitution(fatec);
 			if(sucess){
-				mapa.put(200, "removido com sucesso");
+				res.status(200);
+				return "successfully inserted";
 			}else{
-				mapa.put(400, "codigo fatec inválida");
+				res.status(400);
+				return "failure to enter the institution";
 			}
-			return mapa;
 		}, JsonUtil.json());
 		
-		get("/Institution/findAll", (req, res) -> {
-			Map<Integer, String> mapa = new HashMap<Integer, String>();
+		put("/institution/update", (req, res) -> {
+			String data = req.body();
+			Gson gson = new Gson();
+			Institution fatec = gson.fromJson(data, Institution.class);
+			
+			boolean sucess = model.updateInstitution(fatec);
+			if(sucess){
+				res.status(200);
+				return "successfully changed";
+			}else{
+				res.status(400);
+				return "failure to change institution";
+			}
+		}, JsonUtil.json());
+		
+		get("/institution/find/name", (req, res) -> {
+			String name =  req.queryParams("name") ;
+			List<Institution> fatecs;
+			if( name != null ){
+				fatecs = model.searchInstitutionByName(name);
+				if(fatecs.size() > 0){
+					return fatecs;
+				}else{
+					res.status(404);
+					return "FATEC not found";
+				}
+			}else{
+				res.status(400);
+				return "invalid parameter";
+			} 
+			
+		}, JsonUtil.json());
+		
+		get("/institution/find/code", (req, res) -> {
+			if( req.queryParams("code") == null ){
+				res.status(400);
+				return "invalid parameter";
+			}else{
+				Long code = Long.parseLong( req.queryParams("codeInstitution") );
+				Institution fatec = model.searchInstitutionByCode(code);
+				
+				if( fatec.getCode() == null ){
+					res.status(404);
+					return "FATEC not found";
+				}else{
+					return fatec;
+				}	
+			}
+		}, JsonUtil.json());
+		
+		get("/institution/delete", (req, res) -> {
+			if( req.queryParams("codeInstitution") == null ){
+				res.status(400);
+				return "invalid parameter";
+			}else{
+				Long code = Long.parseLong( req.queryParams("codeInstitution") );
+				boolean sucess = model.deleteInstitution(code);
+				if(sucess){
+					res.status(200);
+					return "successfully removed";
+				}else{
+					res.status(400);
+					return "Invalid FATEC code";
+				}
+			}
+		}, JsonUtil.json());
+		
+		get("/institution/find/all", (req, res) -> {
 			List<Institution> fatecs = model.searchAllInstitution();
 			if(fatecs.size() > 0){
 				return fatecs;
 			}else{
-				mapa.put(400, "nenhuma fatec cadastrada");
-				return mapa;
+				res.status(400);
+				return "no FATEC registered";
 			}
 		}, JsonUtil.json());
-		
-	
 		
 	}
 
