1c9b49aa2285c6219bbe2acc853a23ecb2b21bc3
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-30 22:22:02 -0300

    Create Class DaoInstitutionCourse

---
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/dao/DaoInstitutionCourse.java
@@ -0,0 +1,54 @@
+package br.com.fatec.dao;
+
+import java.sql.SQLException;
+
+import br.com.fatec.connection.ConnectionMySql;
+
+public class DaoInstitutionCourse {
+	
+	@SuppressWarnings("finally")
+	public static boolean insertIntitutionCourse(Long idInstitution, Long idCourse){
+		ConnectionMySql connection = new ConnectionMySql();
+		boolean insert = false;
+		try{
+			String sql = "insert into ist_crs (ist_code, crs_code) values (?,?)";
+			connection.conect();
+			connection.setStatement(connection.getConnection().prepareStatement(sql));
+			connection.getStatement().setLong(1, idInstitution);
+			connection.getStatement().setLong(2, idCourse);
+			if(connection.executeSql()){
+				insert = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			connection.close();
+			return insert;
+		}
+	}
+	
+	@SuppressWarnings("finally")
+	public static boolean deleteIntitutionCourse(Long idInstitution, Long idCourse){
+		ConnectionMySql connection = new ConnectionMySql();
+		boolean delete = false;
+		try{
+			String sql = "delete from ist_crs where ist_code = ? and crs_code = ?";
+			connection.conect();
+			connection.setStatement(connection.getConnection().prepareStatement(sql));
+			connection.getStatement().setLong(1, idInstitution);
+			connection.getStatement().setLong(2, idCourse);
+			if(connection.executeSql()){
+				delete = true;
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			connection.close();
+			return delete;
+		}		
+	}
+	
+	
+}
