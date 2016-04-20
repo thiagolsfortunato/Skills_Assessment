38594bb4145515dafeaa5d58f79a422257b22ec8
Marcelo-Inacio <marcelomcl_10@hotmail.com>
2016-04-20 20:03:50 -0300

    update class corsFilter

--- Project/Back_End/SkillsAssessment/src/br/com/fatec/connection/CorsFilter.java
+++ Project/Back_End/SkillsAssessment/src/br/com/fatec/connection/CorsFilter.java
@@ -1,18 +1,26 @@
 package br.com.fatec.connection;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
+import spark.utils.IOUtils;
 
 public class CorsFilter {
 	 private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
 	    
 	    static {
 	    	corsHeaders.put("Access-Control-Allow-Origin", "*");
-	        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-	        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");     
+	        //corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+	        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,Foo-Headers");
+	        //corsHeaders.put("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Content-Length, Accept-Encoding, Accept-Language, Access-Control-Request-Method, Connection, Host, Origin, Referer, User-Agent, Client-Security-Token");
 	        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+	        corsHeaders.put("Access-Control-Max-Age", "86400");
+	        
+	        corsHeaders.put("Access-Control-Request-Methods", "GET, POST, PUT, DELETE");
+	        //corsHeaders.put("Access-Control-Request-Headers", "X-Requested-With,Access-Control-Request-Method,Access-Control-Request-Headers, accept, Content-Type");
 	    }
 
 	    public final static void apply() {
@@ -24,6 +32,15 @@ public class CorsFilter {
 	                });
 	            }
 	        };
-	        Spark.after(filter);
+	        
+	        Spark.before(filter);
 	    }
+	    
+	        
+
+		public static HashMap<String, String> getCorsheaders() {
+			return corsHeaders;
+		}
+	   
+	    
 }
