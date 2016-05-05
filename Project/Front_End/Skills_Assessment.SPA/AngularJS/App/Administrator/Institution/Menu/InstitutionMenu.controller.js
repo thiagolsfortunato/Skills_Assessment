3c6f4e2730c4eb20a5d8ee307f0e84eadb7338c0
Daniel Barbosa <daniel.bsilveira@outlook.com>
2016-05-04 22:35:24 -0300

    Adicionado opção para listar e excluir na tela de Instituição

--- Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Administrator/Institution/Menu/InstitutionMenu.controller.js
+++ Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Administrator/Institution/Menu/InstitutionMenu.controller.js
@@ -1,13 +1,77 @@
-﻿//PortalLexosControllers.controller('InstitutionMenuController',
-//    ['$scope', '$routeParams', 'InstitutionMenuService', '$interval',
-//            function ($scope, $routeParams, InstitutionMenuService, $interval) {
+﻿publication.factory('InstitutionService', ['$http', '$q', function ($http, $q) {
+
+    return {
+
+        institutionSave: _institutionSave,
+        institutionsList: _institutionsList,
+        institutionDelete: _institutionDelete,
+        institutionUpdate: _institutionUpdate
+
+    };
+
+    function _institutionSave(obj) {
+
+        var deferred = $q.defer();
+
+        $http({
+            method: 'POST',
+            url: config.generateApiUrl('institution'),
+            data: JSON.stringify(obj),
+            headers: { 'content-type': 'application/json' }
+        }).
+           success(function (data, status, headers, config) {
+               deferred.resolve(data);
+           });
+
+        return deferred.promise;
+    }
 
 
-//            init();
+    function _institutionsList() {
 
-//            function init() {
+        var deferred = $q.defer();
 
+        $http({
+            method: 'GET',
+            url: config.generateApiUrl('institution/find/all')
+        }).
+           success(function (data, status, headers, config) {
+               deferred.resolve(data);
+           });
 
-//            }
+        return deferred.promise;
+    }
 
-//        }]);
+    function _institutionDelete(id) {
+
+        var deferred = $q.defer();
+
+        $http({
+            method: 'delete',
+            url: config.generateApiUrl('institution'),
+            params: { "code": id }
+
+        }).
+           success(function (data, status, headers, config) {
+               deferred.resolve(data);
+           });
+
+        return deferred.promise;
+    }
+
+    function _institutionUpdate(dataObj) {
+
+        var deferred = $q.defer();
+
+        $http({
+            method: 'PUT',
+            url: config.generateApiUrl('institution'),
+            data: JSON.stringify(dataObj)
+        }).
+           success(function (data, status, headers, config) {
+               deferred.resolve(dataObj);
+           });
+
+        return deferred.promise;
+    }
+}]);
