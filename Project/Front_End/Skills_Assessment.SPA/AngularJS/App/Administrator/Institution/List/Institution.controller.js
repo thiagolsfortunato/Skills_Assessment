e2194b36db3fe2f74af71868c009deec7010dbd8
Marcelo <marcelomcl_10@hotmail.com>
2016-05-05 15:45:04 -0300

    front end Institution OK

---
+++ Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Administrator/Institution/List/Institution.controller.js
@@ -0,0 +1,84 @@
+﻿FatecControllers.controller('InstitutionController',
+    ['$scope', '$routeParams', 'InstitutionService', '$log', 'localStorageService',
+        function ($scope, $routeParams, institutionService, $log, localStorageService) {
+
+            //Declaração de funções
+            $scope.institutionList = _institutionList;
+            $scope.institutionAdd = _institutionAdd;
+            $scope.institutionDelete = _institutionDelete;
+            $scope.institutionLoad = _institutionLoad;
+            $scope.institutionCancel = _institutionCancel;
+            $scope.institutionUpdate = _institutionUpdate;
+
+            $scope.btnLabel = "Adicionar";
+
+            $scope.institutions;
+            $scope.institution;
+
+
+            init();
+
+            function init() {
+
+                $scope.institutionList();
+            }
+
+            function _institutionCancel() {
+
+                $scope.btnLabel = "Adicionar";
+                $scope.institution = null;
+            }
+
+            function _institutionLoad(obj) {
+
+                $scope.institution = angular.copy(obj);;
+                $scope.btnLabel = "Alterar";
+
+            }
+
+            function _institutionList() {
+
+                institutionService.institutionList().then(function (data) {
+
+                    console.log(data);
+
+                    $scope.institutions = data;
+
+                });
+
+            }
+
+            function _institutionUpdate(institution) {
+                // courseupdate é o obj que chama a função da service
+                console.log(institution);
+                institutionService.institutionUpdate(institution).then(function (data) {
+
+                    $scope.institutionList();
+                    alert("Alterou");
+                });
+
+            }
+
+            function _institutionAdd(institution) {
+                // courseAdd é o obj que chama a função da service
+                console.log(institution);
+                institutionService.institutionSave(institution).then(function (data) {
+
+                    $scope.institutionList();
+                    alert("Salvouuu");
+
+                });
+            }
+
+            function _institutionDelete(id) {
+
+                console.log(id);
+                institutionService.institutionDelete(id).then(function (data) {
+
+                    $scope.institutionList();
+                    alert("Deletouuu");
+
+                });
+            }
+
+        }]);
