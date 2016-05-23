a946a84b7d8b71c58c77ac3045a47c346e9f2bcb
Marcelo <marcelomcl_10@hotmail.com>
2016-05-23 18:58:18 -0300

    scream psicologa
    marcelo

--- Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Psicologa/List/Psicologa.controller.js
+++ Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Psicologa/List/Psicologa.controller.js
@@ -2,7 +2,28 @@
     ['$scope', '$routeParams', 'PsicologaService', '$log',
         function ($scope, $routeParams, psicologaService, $log) {
 
-            $scope.psicologa;
+            $scope.psicologa = {
+                "nome": "nome da psicoloca",
+            };
+
+            $scope.alunos = [
+                { "id": 1, "nome": "pé de muleque", "situacao": "andamento" },
+                { "id": 2, "nome": "zé pequeno", "situacao": "concluido" },
+                { "id": 3, "nome": "aluno 42", "situacao": "parado" }
+            ];
+
+            $scope.cursos = [{ nome: "Aeronáutica" }, { nome: "Gestão Produção" }, { nome: "Logistica" }];
+
+            $scope._ano = [{ ano: "corrente" }, { ano: 2015 }, { ano: 2014 }];
+
+            $scope.cursoSelected;
+            $scope.anoSelected;
+
+            $scope.func = function () {
+                console.log($scope.cursoSelected);
+                console.log($scope.anoSelected);
+            }
+
             // FAZER
  /*           $scope.user = [];
             $scope.userDelete = _userDelete;
