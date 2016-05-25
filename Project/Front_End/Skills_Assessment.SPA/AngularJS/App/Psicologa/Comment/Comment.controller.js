25a8ff5cbab9aaad02b66436015acee08f70e1e1
Marcelo <marcelomcl_10@hotmail.com>
2016-05-25 14:30:53 -0300

    tela psicologa grafico

--- Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Psicologa/Comment/Comment.controller.js
+++ Project/Front_End/Skills_Assessment.SPA/AngularJS/App/Psicologa/Comment/Comment.controller.js
@@ -3,6 +3,26 @@
         function ($scope, $routeParams, psicologaService, $log) {
 
             $scope.psicologa;
+
+            var radarData =
+            {
+                labels: ['Linda', 'Feia', 'Comuna'],
+                datasets: [{
+                    fillColor: "rgba(63,169,245,.1)",
+                    strokeColor: "rgba(63,169,245,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    data: [0, 6000, 7780]
+                }]
+            }
+            var options = {
+                segmentShowStroke: false,
+                animateScale: true
+            }
+
+            var ctx2 = document.getElementById("radarChart").getContext("2d");
+            var myNewChart = new Chart(ctx2).Radar(radarData);
+            new Chart(ctx2).Radar(radarData, options);
             // FAZER
  /*           $scope.user = [];
             $scope.userDelete = _userDelete;
